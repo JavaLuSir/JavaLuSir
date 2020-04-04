fe7e6426b4164e4263ba8cec1ede00e62aa0c67a
JavaLuSir <javalusir@163.com>
2020-04-04 09:04:22 +0800

    增加SQL字典

--- src/T_DICT.sql
+++ src/T_DICT.sql
@@ -1,4 +1,5 @@
 ﻿
+
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -13,3 +14,14 @@ CREATE TABLE `T_DICT` (
   `ORDERNUM` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
   PRIMARY KEY (`KID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of T_DICT
+-- ----------------------------
+INSERT INTO `T_DICT` VALUES ('1', '00', '转账', 'A', '0');
+INSERT INTO `T_DICT` VALUES ('2', '10', '居家', 'A', '0');
+INSERT INTO `T_DICT` VALUES ('3', '20', '食品', 'A', '0');
+INSERT INTO `T_DICT` VALUES ('4', '30', '交通', 'A', '0');
+INSERT INTO `T_DICT` VALUES ('5', '40', '投资', 'A', '0');
+INSERT INTO `T_DICT` VALUES ('6', '50', '还款', 'A', '0');
+INSERT INTO `T_DICT` VALUES ('7', '61', '工资', 'A', '0');
